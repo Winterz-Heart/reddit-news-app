0434af28f6eeea901cbd7507623ba2bb5cee8a09
Winterz-Heart <duncan47685@gmail.com>
2025-12-31 13:16:05 +0000

    Add PostCard and Subreddit styles

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,14 +1,25 @@
 import './PostCard.css'
 
 function PostCard (props) {
+    const created_utc = props.created_utc
+    const date = new Date(created_utc * 1000)
+    const formattedTime = date.toLocaleTimeString()
+    const formattedDate = date.toLocaleDateString()
+
     return (
-        <div>
-            <img src={props.subredditIcon} alt={props.subreddit + ' icon'} />
-            <p> r/{props.subreddit} | {props.author} | {props.created_utc} </p>
-            <p> {props.title} </p>
-            <a href={props.url}> {props.url} </a>
-            <p>Upvote | {props.score} | Downvote</p>
-            <p>Comments | {props.num_comments} </p>
+        <div className='post-card'>
+            <div className='post-card-header'>
+                <img className='subreddit-icon' src={props.subredditIcon} alt={props.subreddit + ' icon'} />
+                <p> r/{props.subreddit} | {props.author} | {formattedTime} {formattedDate} </p>
+            </div>
+            <div className='post-card-content'>
+                <p> {props.title} </p>
+                <a href={props.url} target='_blank' rel="noreferrer" > {props.url} </a>
+            </div>
+            <div className='post-card-footer'>
+                <p>Upvote | {props.score} | Downvote</p>
+                <p>Comments | {props.num_comments} </p>
+            </div>
         </div>
     )
 }
--- src/pages/Subreddit/Subreddit.jsx
+++ src/pages/Subreddit/Subreddit.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectSubreddit, fetchSubreddit } from "./SubredditSlice";
 import { useParams } from "react-router-dom";
 import PostCard from "../../components/PostCard/PostCard";
+import './Subreddit.css'
 
 function Subreddit() {
     const dispatch = useDispatch();
@@ -17,8 +18,16 @@ function Subreddit() {
     const subreddit = useSelector(selectSubreddit);
     const { posts, errors, status } = subreddit;
 
+    const subredditName = posts[0].subreddit
+    const subredditIcon = posts[0].subredditIcon
+    const subredditIconUrl = subredditIcon.split('?')[0]
+
     return (
         <div>
+            <div className="page-header">
+                <img src={subredditIconUrl} alt={subredditName + ' icon'} className="subreddit-header-icon" />
+                <h2>r/{subredditName}</h2>
+            </div>
             <section>
                 {errors ? (
                     <p>Something went wrong!</p>
